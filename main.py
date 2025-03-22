49b0c1b5ccc164144eeecbae838c412c40973dfb
braed <braedenmichaeljones@gmail.com>
2025-03-22 18:11:59 -0500

    add player and randomly moving asteroids

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
     pygame.init()
-
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    asteroid_field = AsteroidField()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,7 +29,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        for i in drawable:
+            i.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
